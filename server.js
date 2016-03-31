3df7edb775edd4753c3e96cc6c5dc9c8cf5127fc
tammerg <tammergalal20@gmail.com>
2016-03-30 21:04:24 -0400

    initial commit, added public folder and created and filled out initial server.js file, created app.js, and will begin the basic html landing page now

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+PORT = process.env.PORT || 8080;
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+
+app.get('*', function(req, res){
+  res.sendFile(process.cwd()+ 'public/index.html');
+});
+
+app.listen(PORT, function(){
+  console.log('Connected on ', PORT);
+});
